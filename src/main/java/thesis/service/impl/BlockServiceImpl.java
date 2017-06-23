d444e77a0e6026814b31d734e63af8a138719c1a
Alexei Zamyatin <alexei.zamyatin@tuwien.ac.at>
2017-06-23 13:45:17 +0200

    Configured update runs

--- src/main/java/thesis/service/impl/BlockServiceImpl.java
+++ src/main/java/thesis/service/impl/BlockServiceImpl.java
@@ -1,6 +1,7 @@
 package thesis.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import thesis.exception.ServiceException;
@@ -14,10 +15,15 @@ public class BlockServiceImpl implements BlockService {
     @Autowired
     private BlockRepository repository;
 
+
     @Override
     @Transactional
     public Block create(Block toCreate) throws ServiceException {
-        return repository.save(toCreate);
+        //
+        if(toCreate.getTime() <= 1497830400)
+            return repository.save(toCreate);
+        else
+            return toCreate;
     }
 
     @Override
